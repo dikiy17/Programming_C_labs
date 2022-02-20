d63cf2500679feca2d990c6b0267572a3112d915
dikiy <kn1b21.dykyi@kpnu.edu.ua>
2022-02-20 22:47:35 +0200

    lab0104

---
+++ lab0104/lab0104.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "first number" << endl;
+    cin >> a;
+    cout << "second number" << endl;
+    cin >> b;
+    cout << a * a + b * b;
+    cout << a + b;
+    return 0;
+}
